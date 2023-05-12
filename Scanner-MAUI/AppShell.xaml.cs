3c89a6922b90db9824a3eb2ca75ef8c95030bc81
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-05-12 09:50:37 +0300

    Add project files.

---
+++ Scanner-MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Scanner_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Scanner-MAUI/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace Scanner_MAUI;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
