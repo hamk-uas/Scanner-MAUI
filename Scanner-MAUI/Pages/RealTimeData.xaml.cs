adece61a8c1d7b792172a5073e866cd1a29c1a93
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-05-29 15:56:34 +0300

    Dyncamic network name base on seleterd network

--- Scanner-MAUI/Pages/RealTimeData.xaml.cs
+++ Scanner-MAUI/Pages/RealTimeData.xaml.cs
@@ -1,10 +1,23 @@
-namespace Scanner_MAUI.Pages;
 
-public partial class RealTimeData : ContentPage
+using System.Diagnostics.CodeAnalysis;
+
+namespace Scanner_MAUI.Pages
 {
-	public RealTimeData()
-	{
-		InitializeComponent();
+    public partial class RealTimeData : ContentPage
+    {
+        public RealTimeData()
+        {
+            InitializeComponent();
+            NetworkListView.ItemSelected += OnNetworkNameSelected;
+        }
 
-	}
+        //Dynamically populating the network name
+        private void OnNetworkNameSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem is string selectedNetwork)
+            {
+                NetworkNameLabel.Text = $"Network Name: {selectedNetwork}";
+            }
+        }
+    }
 }
