ec6e9a9f49be019f168804c49338afa5674063dd
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-06-08 22:01:50 +0300

    WMTS maps implemented using maanmittaulaitos open api. API key fetched from user secrets local folder

---
+++ Scanner-MAUI/Helpers/ViewMap.cs
@@ -0,0 +1,61 @@
+﻿using Esri.ArcGISRuntime.Mapping;
+using Map = Esri.ArcGISRuntime.Mapping.Map;
+using Esri.ArcGISRuntime.Ogc;
+using Esri.ArcGISRuntime.Maui;
+using Scanner_MAUI.Helpers;
+
+namespace Scanner_MAUI.Helpers
+{
+    public static class ViewMap
+    {
+        public static async Task LoadWMTSLayer(MapView MyMapView)
+        {
+            try
+            {
+                //get value by key
+                //var apiKey = Keys.GetApiKey();
+                var apiKey = Keys.Instance["Settings:API-key"];
+
+                // Create a new map.
+                Map myMap = new();
+
+                // Get the basemap from the map.
+                Basemap myBasemap = myMap.Basemap;
+
+                // Get the layer collection for the base layers.
+                LayerCollection myLayerCollection = myBasemap.BaseLayers;
+
+                // Create an instance for the WMTS layer.
+                WmtsLayer myWmtsLayer;
+
+                // Define the Uri to the WMTS service.
+                Uri wmtsUri = new("https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key=" + apiKey);
+
+                // Define a new instance of the WMTS service.
+                WmtsService myWmtsService = new(wmtsUri);
+
+                // Load the WMTS service.
+                await myWmtsService.LoadAsync();
+
+                // Get the service information (i.e., metadata) about the WMTS service.
+                WmtsServiceInfo myWmtsServiceInfo = myWmtsService.ServiceInfo;
+
+                // Obtain the read-only list of WMTS layer info objects and select the one with the desired Id value.
+                WmtsLayerInfo info = myWmtsServiceInfo.LayerInfos.Single(l => l.Id == "taustakartta");
+
+                // Create a WMTS layer using WMTS layer info.
+                myWmtsLayer = new WmtsLayer(info);
+
+                // Add the WMTS layer to the layer collection of the map.
+                myLayerCollection.Add(myWmtsLayer);
+
+                // Assign the map to the MapView.
+                MyMapView.Map = myMap;
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", ex.ToString(), "OK");
+            }
+        }
+    }
+}
--- Scanner-MAUI/MauiProgram.cs
+++ Scanner-MAUI/MauiProgram.cs
@@ -2,6 +2,8 @@
 using Esri.ArcGISRuntime.Maui;
 using Esri.ArcGISRuntime.Toolkit.Maui;
 using Esri.ArcGISRuntime;
+using Scanner_MAUI.Helpers;
+using Esri.ArcGISRuntime.Security;
 
 namespace Scanner_MAUI;
 
@@ -10,6 +12,7 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+		var apiKey = Keys.Instance["Settings:API-key"];
 		builder
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
@@ -17,11 +20,17 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			})
-			.UseArcGISRuntime(/*config => config.UseApiKey("")*/)
-			.UseArcGISToolkit();
+            .UseArcGISRuntime()
+            //.UseArcGISRuntime(config => config.UseApiKey("apiKey"))
+            //.UseArcGISRuntime(config => config
+            //	.ConfigureAuthentication(auth => auth
+            //		.UseDefaultChallengeHandler()
+            //	)
+            //)
+            .UseArcGISToolkit();
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
