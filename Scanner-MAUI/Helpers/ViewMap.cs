c9764d1b4310811322aa81b9c115cf5cdd879e0d
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-06-09 21:41:17 +0300

    Basic header authentcation implemented

--- Scanner-MAUI/Helpers/ViewMap.cs
+++ Scanner-MAUI/Helpers/ViewMap.cs
@@ -3,6 +3,8 @@ using Map = Esri.ArcGISRuntime.Mapping.Map;
 using Esri.ArcGISRuntime.Ogc;
 using Esri.ArcGISRuntime.Maui;
 using Scanner_MAUI.Helpers;
+using System.Net.Http.Headers;
+using System.Net;
 
 namespace Scanner_MAUI.Helpers
 {
@@ -27,29 +29,58 @@ namespace Scanner_MAUI.Helpers
                 // Create an instance for the WMTS layer.
                 WmtsLayer myWmtsLayer;
 
+                // Create a new HttpClient.
+                HttpClient httpClient = new HttpClient();
+
+                //specify to use TLS 1.2 as default connection (Maube not needed)
+                System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
                 // Define the Uri to the WMTS service.
                 Uri wmtsUri = new("https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key=" + apiKey);
 
-                // Define a new instance of the WMTS service.
-                WmtsService myWmtsService = new(wmtsUri);
+                // Set the base address of the HttpClient.
+                httpClient.BaseAddress = wmtsUri;
 
-                // Load the WMTS service.
-                await myWmtsService.LoadAsync();
+                // Attach the Authorization header with the API key as the username.
+                string authHeader = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(apiKey + ":"));
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
 
-                // Get the service information (i.e., metadata) about the WMTS service.
-                WmtsServiceInfo myWmtsServiceInfo = myWmtsService.ServiceInfo;
+                // Create a new HttpRequestMessage.
+                HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, wmtsUri);
 
-                // Obtain the read-only list of WMTS layer info objects and select the one with the desired Id value.
-                WmtsLayerInfo info = myWmtsServiceInfo.LayerInfos.Single(l => l.Id == "taustakartta");
+                // Send the request and get the response.
+                HttpResponseMessage response = await httpClient.SendAsync(request);
 
-                // Create a WMTS layer using WMTS layer info.
-                myWmtsLayer = new WmtsLayer(info);
+                // Check if the request was successful.
+                if (response.IsSuccessStatusCode)
+                {
+                    // Define a new instance of the WMTS service.
+                    WmtsService myWmtsService = new(wmtsUri);
 
-                // Add the WMTS layer to the layer collection of the map.
-                myLayerCollection.Add(myWmtsLayer);
+                    // Load the WMTS service.
+                    await myWmtsService.LoadAsync();
 
-                // Assign the map to the MapView.
-                MyMapView.Map = myMap;
+                    // Get the service information (i.e., metadata) about the WMTS service.
+                    WmtsServiceInfo myWmtsServiceInfo = myWmtsService.ServiceInfo;
+
+                    // Obtain the read-only list of WMTS layer info objects and select the one with the desired Id value.
+                    WmtsLayerInfo info = myWmtsServiceInfo.LayerInfos.Single(l => l.Id == "taustakartta");
+
+                    // Create a WMTS layer using WMTS layer info.
+                    myWmtsLayer = new WmtsLayer(info);
+
+                    // Add the WMTS layer to the layer collection of the map.
+                    myLayerCollection.Add(myWmtsLayer);
+
+                    // Assign the map to the MapView.
+                    MyMapView.Map = myMap;
+                }
+                else
+                {
+                    // Handle the error response.
+                    string errorMessage = $"Request failed with status code {response.StatusCode}: {response.ReasonPhrase}";
+                    await Application.Current.MainPage.DisplayAlert("Error", errorMessage, "OK");
+                }
             }
             catch (Exception ex)
             {
