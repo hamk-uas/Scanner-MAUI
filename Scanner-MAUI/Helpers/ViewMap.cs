c9b00404fed7ad362a19a19a1bc7a223c109ef5a
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-06-13 09:11:02 +0300

    Different methods to load the map

--- Scanner-MAUI/Helpers/ViewMap.cs
+++ Scanner-MAUI/Helpers/ViewMap.cs
@@ -5,17 +5,24 @@ using Esri.ArcGISRuntime.Maui;
 using Scanner_MAUI.Helpers;
 using System.Net.Http.Headers;
 using System.Net;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using Windows.Media.Protection.PlayReady;
 
 namespace Scanner_MAUI.Helpers
 {
     public static class ViewMap
     {
+
         public static async Task LoadWMTSLayer(MapView MyMapView)
         {
             try
             {
                 //get value by key
                 var apiKey = Keys.Instance["Settings:API-key"];
+
+                _ = apiKey;
 
                 // Create a new map.
                 Map myMap = new();
@@ -29,28 +36,37 @@ namespace Scanner_MAUI.Helpers
                 // Create an instance for the WMTS layer.
                 WmtsLayer myWmtsLayer;
 
+                //specify to use TLS 1.2 as default connection (Maube not needed)
+                //System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+                // Define the Uri to the WMTS service.
+                //Uri wmtsUri = new("https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key=" + apiKey);
+
+                // Define the Uri to the WMTS service.
+                Uri wmtsUri = new("https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/taustakartta/default/ETRS-TM35FIN/3/5/3.png?api-key=" + apiKey);
+
+                // Attach the Authorization header with the API key as the username.
+                string headAuth = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{apiKey}:"));
+
                 // Create a new HttpClient.
                 HttpClient httpClient = new HttpClient();
 
-                //specify to use TLS 1.2 as default connection (Maube not needed)
-                System.Net.ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-
-                // Define the Uri to the WMTS service.
-                Uri wmtsUri = new("https://avoin-karttakuva.maanmittauslaitos.fi/avoin/wmts/1.0.0/WMTSCapabilities.xml?api-key=" + apiKey);
-
                 // Set the base address of the HttpClient.
                 httpClient.BaseAddress = wmtsUri;
-
-                // Attach the Authorization header with the API key as the username.
-                string authHeader = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(apiKey + ":"));
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", headAuth);
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 // Create a new HttpRequestMessage.
                 HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, wmtsUri);
                 //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, wmtsUri);
 
+                //request.Headers.Add("Authorization", "Basic " + user_id);
+                //httpClient.Send(request);
+
                 // Send the request and get the response.
                 HttpResponseMessage response = await httpClient.SendAsync(request);
+                //var response = httpClient.GetAsync(wmtsUri).Result;
+
 
                 // Check if the request was successful.
                 if (response.IsSuccessStatusCode)
@@ -58,29 +74,35 @@ namespace Scanner_MAUI.Helpers
                     // Get the response content.
                     HttpContent content = response.Content;
 
-                    // Read the response as a string.
-                    string responseString = await content.ReadAsStringAsync();
+                    //    // Read the response as a string.
+                    //    string responseString = await content.ReadAsStringAsync();
 
-                    // Define a new instance of the WMTS service.
-                    WmtsService myWmtsService = new(wmtsUri);
+                    //// Define a new instance of the WMTS service.
+                    //WmtsService myWmtsService = new(wmtsUri);
 
-                    // Load the WMTS service.
-                    await myWmtsService.LoadAsync();
+                    //// Load the WMTS service.
+                    //await myWmtsService.LoadAsync();
 
-                    // Get the service information (i.e., metadata) about the WMTS service.
-                    WmtsServiceInfo myWmtsServiceInfo = myWmtsService.ServiceInfo;
+                    //// Get the service information (i.e., metadata) about the WMTS service.
+                    //WmtsServiceInfo myWmtsServiceInfo = myWmtsService.ServiceInfo;
 
-                    // Obtain the read-only list of WMTS layer info objects and select the one with the desired Id value.
-                    WmtsLayerInfo info = myWmtsServiceInfo.LayerInfos.Single(l => l.Id == "taustakartta");
+                    //// Obtain the read-only list of WMTS layer info objects and select the one with the desired Id value.
+                    //WmtsLayerInfo info = myWmtsServiceInfo.LayerInfos.Single(l => l.Id == "taustakartta");
 
-                    // Create a WMTS layer using WMTS layer info.
-                    myWmtsLayer = new WmtsLayer(info);
+                    //// Create a WMTS layer using WMTS layer info.
+                    //myWmtsLayer = new WmtsLayer(info);
+
+                    // Create a WMTS layer using a Uri and provide an Id value.
+                    myWmtsLayer = new WmtsLayer(wmtsUri, "taustakartta");
 
                     // Add the WMTS layer to the layer collection of the map.
                     myLayerCollection.Add(myWmtsLayer);
 
                     // Assign the map to the MapView.
                     MyMapView.Map = myMap;
+
+                    var Test = "This is a test to check if the code is being executed untill this point";
+                  
                 }
                 else
                 {
--- Scanner-MAUI/MauiProgram.cs
+++ Scanner-MAUI/MauiProgram.cs
@@ -20,7 +20,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			})
-			.UseArcGISRuntime()
+			//.UseArcGISRuntime()
 			//.UseArcGISRuntime(config => config.UseApiKey(apiKey))
 			//.UseArcGISRuntime(config => config
 			//	.ConfigureAuthentication(auth => auth
