70eff1e5994b677b3099e953b19efb05e9d84501
Basel <107993017+BasilOmsha@users.noreply.github.com>
2023-06-22 16:19:21 +0300

    connection to serial port established, command set successfully, and buffer data read

---
+++ Scanner-MAUI/Helpers/serialPortConn.cs
@@ -0,0 +1,101 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.IO.Ports;
+//using System.Threading.Tasks;
+//using System.Diagnostics;
+
+//namespace Scanner_MAUI.Helpers
+//{
+//    class SerialPortConn
+//    {
+//        private SerialPort _serialPort;
+//        private int _inputMode;
+//        private bool _continue;
+
+//        public void ScannerData()
+//        {
+//            _serialPort = new SerialPort("COM6", 115200, Parity.None, 8, StopBits.One);
+//            _serialPort.Handshake = Handshake.XOnXOff;
+//            if (!_serialPort.IsOpen)
+//            {
+//                _serialPort.Open();
+//                Debug.WriteLine("Serial port connected.");
+//                _serialPort.WriteTimeout = 1000;
+//                _serialPort.Write("1");
+//                _serialPort.WriteLine("");
+//                Debug.WriteLine("Serial port connected and input given.");
+//                //_serialPort.ReadTimeout = 8000;
+                
+//                while (true)
+//                {
+//                    try
+//                    {
+//                        string receivedData = _serialPort.ReadLine().Trim();
+//                        //string packetString = Convert.ToString(packet);
+//                        Debug.WriteLine("packet string: " + receivedData);
+//                        string decodedPacket = Encoding.UTF8.GetString(Encoding.Default.GetBytes(receivedData)).TrimEnd('\n');
+
+//                        Debug.WriteLine("Decodedpacket string: " + decodedPacket);
+
+//                        //string message = _serialPort.ReadLine();
+//                        //string decodedMessage = Encoding.UTF8.GetString(Encoding.Default.GetBytes(message));
+//                        Debug.WriteLine("Received: " + decodedPacket);
+//                        Debug.WriteLine("Reading....");
+
+//                        string name = ExtractValue(decodedPacket, "name:");
+//                        string type = ExtractValue(decodedPacket, "type:");
+//                        string rssi = ExtractValue(decodedPacket, "rssi:");
+//                        string snr = ExtractValue(decodedPacket, "snr:");
+//                        string time = ExtractValue(decodedPacket, "time:");
+
+//                        // Perform any processing or display the extracted information
+//                        Debug.WriteLine("Name: " + name);
+//                        Debug.WriteLine("Type: " + type);
+//                        Debug.WriteLine("RSSI: " + rssi);
+//                        Debug.WriteLine("SNR: " + snr);
+//                        Debug.WriteLine("Time: " + time);
+
+                           
+                    
+                        
+//                    }
+//                    catch (TimeoutException ex)
+//                    {
+//                        // Handle the timeout exception
+//                        Debug.WriteLine("Timeout occurred while reading from the serial port: " + ex.Message);
+//                    }
+//                }
+              
+//            }
+
+//            // Close the serial port when done
+//            _serialPort.Close();
+//            Debug.WriteLine("Serial port connection closed!");
+//        }
+
+//        public void SendInput(int input)
+//        {
+//            // Make sure the serial port is open before sending the input
+//            if (_serialPort.IsOpen)
+//            {
+//                _serialPort.Write(input.ToString());
+//                if (input == 1)
+//                {
+//                    _inputMode = 1;
+//                }
+//            }
+//        }
+
+//        private string ExtractValue(string message, string keyword)
+//        {
+//            int startIndex = message.IndexOf(keyword) + keyword.Length;
+//            int endIndex = message.IndexOf(",", startIndex);
+//            if (endIndex == -1)
+//                endIndex = message.Length;
+
+//            return message.Substring(startIndex, endIndex - startIndex).Trim();
+//        }
+//    }
+//}
--- Scanner-MAUI/Pages/RealTimeData.xaml.cs
+++ Scanner-MAUI/Pages/RealTimeData.xaml.cs
@@ -9,6 +9,13 @@ using Microsoft.UI.Input;
 using Microsoft.UI.Xaml;
 using System.Reflection;
 using PointerEventArgs = Microsoft.Maui.Controls.PointerEventArgs;
+using System.IO.Ports;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using MenuFlyoutItem = Microsoft.Maui.Controls.MenuFlyoutItem;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+using Esri.ArcGISRuntime.Data;
 
 namespace Scanner_MAUI.Pages;
 
@@ -16,6 +23,9 @@ public partial class RealTimeData : ContentPage
 {
     private GraphicsDrawable graphicsDrawable;
     private Markers mapMarkers;
+    //private SerialPortConn scannerData;
+    private SerialPort serialPort;
+
 
     public RealTimeData()
     {
@@ -27,6 +37,8 @@ public partial class RealTimeData : ContentPage
         _ = ViewMap.LoadWMTSLayer(MyMapView);
         _ = Location.StartDeviceLocationTask(MyMapView); // Gets current location
         //_ = Markers.MapMarkers(MyMapView);
+        //scannerConn = new SerialPortConn();
+        //scannerConn.ScannerData();
     }
 
     // Dynamically populating the network name based on the selected network from the list view
@@ -106,5 +118,171 @@ public partial class RealTimeData : ContentPage
 
         _ = Location.StartDeviceLocationTask(MyMapView);
     }
+
+    //protected override void OnAppearing()
+    //{
+    //    base.OnAppearing();
+
+    //    // Event handler for the "Start" menu item
+    //    var startMenuItem = (MenuFlyoutItem)FindByName("StartMenuItem");
+    //    startMenuItem.Clicked += StartMenuItem_Clicked;
+    //}
+
+    private void StartMenuItem_Clicked(object sender, EventArgs e)
+    {
+
+        // TODO: Connect to the LoRa scanner and start scanning
+        ConnectToScanner();
+
+    }
+
+    private void ConnectToScanner()
+    {
+        try
+        {
+            serialPort = new SerialPort("COM6", 115200);
+            //serialPort.ReadTimeout = 80000;
+            //serialPort.WriteTimeout = 80000;
+            serialPort.Open();
+            Debug.WriteLine("Serial Port conn created");
+            Debug.WriteLine("Serial Port Is Open: " + serialPort.IsOpen);
+
+            var data = new byte[] {(byte)'1', 13 };
+            serialPort.Write(data, 0, data.Length);
+     
+            Debug.WriteLine("test");
+            // TODO: Handle the scanner's output and display the information in your app
+            serialPort.DataReceived += SerialPort_DataReceived;
+            Debug.WriteLine("test2");
+        }
+        catch (Exception ex)
+        {
+            // Handle the exception
+            Debug.WriteLine($"Failed to connect to the scanner: {ex.Message}");
+        }
+    }
+
+    private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    {
+        //try
+        //{
+        //    // Handle the received data from the scanner
+        //    string data = serialPort.ReadLine();
+        //    // Process the data as needed
+        //    ProcessReceivedData(data);
+        //    Debug.WriteLine("Data received from the scanner: " + data);
+        //}
+        try
+        {
+            while (serialPort.BytesToRead > 0)
+            {
+                string DataIn = serialPort.ReadLine();
+                Debug.WriteLine("Data received from the scanner: " + DataIn);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // Handle the cancellation if needed
+            Debug.WriteLine("Reading data from the scanner was canceled.");
+        }
+        catch (Exception ex)
+        {
+            // Handle other exceptions
+            Debug.WriteLine($"Failed to read data from the scanner: {ex.Message}");
+        }
+    }
+
+    //private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+    //{
+    //    try
+    //    {
+    //        // Read the available bytes from the serial port
+    //        int bytesToRead = serialPort.BytesToRead;
+    //        byte[] buffer = new byte[bytesToRead];
+    //        serialPort.Read(buffer, 0, bytesToRead);
+
+    //        // Convert the received bytes to a string
+    //        string data = Encoding.ASCII.GetString(buffer);
+
+    //        // Split the data into separate messages
+    //        string[] messages = data.Split(new[] { "message: datastearm," }, StringSplitOptions.RemoveEmptyEntries);
+
+    //        // Process each message separately
+    //        foreach (string message in messages)
+    //        {
+    //            // Remove leading and trailing whitespace
+    //            string trimmedMessage = message.Trim();
+
+    //            // Process the received data
+    //            ProcessReceivedData(trimmedMessage);
+    //        }
+    //    }
+    //    catch (Exception ex)
+    //    {
+    //        // Handle exceptions
+    //        Debug.WriteLine($"Failed to read data from the scanner: {ex.Message}");
+    //    }
+    //}
+
+    private void ProcessReceivedData(string data)
+    {
+        // Parse and extract relevant information from the data string
+        // Example: Parse data and display specific fields in the console
+        var fields = data.Split(',');
+        if (fields.Length >= 8)
+        {
+            string name = fields[1].Trim();
+            string type = fields[3].Trim();
+            double? latitude = ParseNullableDouble(fields[4].Trim());
+            double? longitude = ParseNullableDouble(fields[5].Trim());
+            int rssi = int.Parse(fields[6].Trim());
+            double snr = double.Parse(fields[7].Trim());
+
+            Debug.WriteLine($"Name: {name}, Type: {type}, Latitude: {latitude}, Longitude: {longitude}, RSSI: {rssi}, SNR: {snr}");
+        }
+        //string[] fields = data.Split(',');
+
+        //if (fields.Length >= 8)
+        //{
+        //    string name = fields[1].Split(':')[1].Trim();
+        //    string type = fields[2].Split(':')[1].Trim();
+        //    double? latitude = ParseNullableDouble(fields[3].Split(':')[1].Trim());
+        //    double? longitude = ParseNullableDouble(fields[4].Split(':')[1].Trim());
+        //    int rssi = int.Parse(fields[5].Split(':')[1].Trim());
+        //    double snr = double.Parse(fields[6].Split(':')[1].Trim());
+
+        //    Debug.WriteLine($"Name: {name}, Type: {type}, Latitude: {latitude}, Longitude: {longitude}, RSSI: {rssi}, SNR: {snr}");
+        //}
+    }
+
+    private double? ParseNullableDouble(string value)
+    {
+        if (double.TryParse(value, out double result))
+            return result;
+
+        return null;
+    }
+
+    private void StopMenuItem_Clicked(object sender, EventArgs e)
+    {
+        // Stop scanning and close the serial port
+        if (serialPort != null && serialPort.IsOpen)
+        {
+            serialPort.WriteLine("0"); // Send the command to stop scanning
+            serialPort.Close();
+            Debug.WriteLine("Serial Port conn closed");
+        }
+    }
+
+    //private async void StartMenuItem_Clicked(object sender, EventArgs e)
+    //{
+    //    scannerData = new SerialPortConn();
+    //    await Task.Run(() =>
+    //    {
+    //        scannerData.ScannerData();
+    //    });
+
+    //    //scannerData.SendInput(1);
+    //}
 }
 
